311403de9b889b53672e04670d273df3a45512db
saimoomedits <ifthakersaimoom200@gmail.com>
2022-04-12 02:43:07 +0400

    init

---
+++ colors/catppuccin.lua
@@ -0,0 +1,32 @@
+-----------------------------------
+-- catppuccin theme for lite-xl! --
+-----------------------------------
+
+local style = require "core.style"
+local common = require "core.common"
+
+style.background = { common.color "#161320" }
+style.background2 = { common.color "#1a1826" }
+style.background3 = { common.color "#302d41" }
+style.text = { common.color "#d9e0ee" }
+style.caret = { common.color "#d9e0ee" }
+style.accent = { common.color "#96cdfb" }
+style.dim = { common.color "#6e6c7e" }
+style.divider = { common.color "#565167" }
+style.selection = { common.color "#302d41" }
+style.line_number = { common.color "#575268" }
+style.line_number2 = { common.color "#d9e0ee" }
+style.line_highlight = { common.color "#1e1e2e" }
+style.scrollbar = { common.color "#575268" }
+style.scrollbar2 = { common.color "#e8a2af" }
+
+style.syntax["normal"] = { common.color "#d9e0ee" }
+style.syntax["symbol"] = { common.color "#d9e0ee" }
+style.syntax["comment"] = { common.color "#6e6c7e" }
+style.syntax["keyword"] = { common.color "#f5c2e7" }
+style.syntax["keyword2"] = { common.color "#f5c2e7" }
+style.syntax["number"] = { common.color "#ddb6f2" }
+style.syntax["literal"] = { common.color "#fae3b0" }
+style.syntax["string"] = { common.color "#fae3b0" }
+style.syntax["operator"] = { common.color "#e8a2af" }
+style.syntax["function"] = { common.color "#abe9b3" }
